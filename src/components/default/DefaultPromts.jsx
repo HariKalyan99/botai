7e745345f6d821b17792a42a63d02c886e6412c1
Harikalyan99 <harikalyan99tronics@gmail.com>
2024-05-08 21:59:40 +0530

    to prod!

--- src/components/default/DefaultPromts.jsx
+++ src/components/default/DefaultPromts.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import logoMain from "../../assets/logo.svg";
 import BasicQuestions from '../basicquestions/BasicQuestions';
 
-const DefaultPromts = ({handleQuestion, entryText, handleGeo}) => {
+const DefaultPromts = ({handleQuestion, entryText}) => {
 
   if(entryText === "Conversation History"){
     return (
@@ -11,7 +11,7 @@ const DefaultPromts = ({handleQuestion, entryText, handleGeo}) => {
           <div className="d-flex flex-column justify-content-center align-items-center pt-5 pb-5" >
             <div className="pt-5">
               <Typography
-                sx={{ fontSize: "28px", fontWeight: "bold" }}
+                sx={{ fontSize: "28px", fontWeight: "bold", textAlign: "center" }}
               >
                 {entryText}
               </Typography>
@@ -31,13 +31,18 @@ const DefaultPromts = ({handleQuestion, entryText, handleGeo}) => {
               >
                 {entryText}
               </Typography>
+              <Typography
+                sx={{ fontSize: "10px", textAlign: "center", color: "red"}}
+              >
+                Note: {"kindly clear your local storage, for security concerns, before exiting!!"}
+              </Typography>
             </div>
             <div className="pt-2 pb-5">
               <img src={logoMain} alt="logo" />
             </div>
           </div>
           <div className="pt-5 pb-5 mt-3">
-            <BasicQuestions handleQuestion={handleQuestion} handleGeo={handleGeo}/>
+            <BasicQuestions handleQuestion={handleQuestion} />
           </div>
       </div>
     )
